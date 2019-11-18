28cfe2bee63f20687a3d05fa05f0f9dda57da159
yxr <2856712933@qq.com>
2019-11-18 08:49:59 +0800

    1

--- inward/src/main/java/com/internal/playment/inward/channel/shortcut/AllinPayChannelHandlePortComponent.java
+++ inward/src/main/java/com/internal/playment/inward/channel/shortcut/AllinPayChannelHandlePortComponent.java
@@ -1,7 +1,7 @@
 package com.internal.playment.inward.channel.shortcut;
 
-
 import com.internal.playment.inward.channel.ChannelHandlePortComponent;
 
 public interface AllinPayChannelHandlePortComponent extends ChannelHandlePortComponent {
+
 }
--- inward/src/main/java/com/internal/playment/inward/service/channel/impl/AllinPayChannelHandlePortServiceImpl.java
+++ inward/src/main/java/com/internal/playment/inward/service/channel/impl/AllinPayChannelHandlePortServiceImpl.java
@@ -1,5 +1,6 @@
 package com.internal.playment.inward.service.channel.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.internal.playment.common.dto.CrossResponseMsgDTO;
 import com.internal.playment.common.enums.StatusEnum;
 import com.internal.playment.common.table.business.PayOrderInfoTable;
@@ -16,10 +17,15 @@ public class AllinPayChannelHandlePortServiceImpl extends ChannelCommonServiceAb
     @Override
     public void successByPayOrder(PayOrderInfoTable payOrderInfoTable, CrossResponseMsgDTO crossResponseMsgDTO) throws Exception {
         int payOrderStatus = payOrderInfoTable.getStatus();
-        //说明同步返回的结果是一样的；什么也不处理
-        if( payOrderStatus == 7 ) { } else {
-            //其他状态改为队列处理中
-            payOrderInfoTable.setStatus(StatusEnum._7.getStatus());
+        //说明与同步返回的结果是一样的；什么也不处理
+        if( payOrderStatus == 7 ) { }
+        //其他状态改为队列处理中
+        else {
+            payOrderInfoTable
+                    .setCrossRespResult(JSON.toJSONString(crossResponseMsgDTO))
+                    .setChannelRespResult(crossResponseMsgDTO.getChannelResponseMsg())
+                    .setUpdateTime(new Date())
+                    .setStatus(StatusEnum._7.getStatus());
             dbCommonRPCComponent.apiPayOrderInfoService.updateByPrimaryKey(payOrderInfoTable);
             activeMqOrderProducerComponent.sendMessage(payOrder, payOrderInfoTable);//发生到钱包队列处理
         }
@@ -31,14 +37,30 @@ public class AllinPayChannelHandlePortServiceImpl extends ChannelCommonServiceAb
         activeMqOrderProducerComponent.sendMessage(asyncNotify,asyncNotifyTable);
     }
 
-
-
-
-
-
     @Override
     public void fieldByPayOrder(PayOrderInfoTable payOrderInfoTable, CrossResponseMsgDTO crossResponseMsgDTO) {
+        int payOrderStatus = payOrderInfoTable.getStatus();
+        //同步状态为1，异步状态为1，一致什么也不处理
+        if( payOrderStatus == 1){}
+        // 同步状态为0，异步状态为1
+        else if( payOrderStatus == 7 ){
+          //做对冲，生成一个新的订单
 
+        }else{//其他状态全部更新为失败
+            payOrderInfoTable
+                    .setCrossRespResult(JSON.toJSONString(crossResponseMsgDTO))
+                    .setChannelRespResult(crossResponseMsgDTO.getChannelResponseMsg())
+                    .setUpdateTime(new Date())
+                    .setStatus(StatusEnum._1.getStatus());
+            //更新数据库表信息
+            dbCommonRPCComponent.apiPayOrderInfoService.updateByPrimaryKey(payOrderInfoTable);
+        }
+        //封装数据库保存对象
+        AsyncNotifyTable asyncNotifyTable = packageAsyncNotifyTable(payOrderInfoTable,StatusEnum._1.getStatus());
+        //保存信息
+        dbCommonRPCComponent.apiAsyncNotifyService.save(asyncNotifyTable);
+        //放到队列执行
+        activeMqOrderProducerComponent.sendMessage(asyncNotify,asyncNotifyTable);
     }
 
 
